141e088535d5cc85eb76e416ab1c9710bbb6d047
inktrap <inktrap@inktrap.org>
2022-07-11 13:37:33 +0200

    update README + reformat python/markdown

--- quantizer/interface.py
+++ quantizer/interface.py
@@ -1,81 +1,94 @@
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtCore import * 
-from PyQt5.QtGui import *
-from PyQt5.Qt import Qt
-import quantizer
-import keyboard
-import time
 import sys
+import time
+
+import keyboard
+from PyQt5 import QtWidgets, uic
+from PyQt5.Qt import Qt
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+import quantizer
+
 
 # Class responsible for opening new threads for processes
 class Worker(QRunnable):
-	def __init__(self, func, *args, **kwargs):
-		super(Worker, self).__init__()
-		self.func = func
-		self.args = args
-		self.kwargs = kwargs
-	
-	@pyqtSlot()
-	def run(self):
-		self.func(*self.args, **self.kwargs)
-	
+    def __init__(self, func, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    @pyqtSlot()
+    def run(self):
+        self.func(*self.args, **self.kwargs)
+
+
 # Main UI class
 class UI(QtWidgets.QMainWindow):
-	def __init__(self):
-		super(UI, self).__init__()
-		uic.loadUi('ui.ui', self)
-		
-		self.threadpool = QThreadPool()
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi("ui.ui", self)
 
-		self.sample_pack_path = None
-		
-		self.load_sample_pack_button = self.findChild(QtWidgets.QPushButton, 'load_sample_pack_button')
-		self.tempo_label = self.findChild(QtWidgets.QLabel, 'tempo_label')
-		self.tempo_slider = self.findChild(QtWidgets.QSlider, 'tempo_slider')
-		self.metronome_radio_button = self.findChild(QtWidgets.QCheckBox, 'metronome_radio_button')
-		self.quantize_radio_button = self.findChild(QtWidgets.QCheckBox, 'quantize_radio_button')
-		self.play_button = self.findChild(QtWidgets.QPushButton, 'playtronic_button')
+        self.threadpool = QThreadPool()
 
-		self.tempo_slider.valueChanged.connect(self.tempo_value)
-		self.load_sample_pack_button.clicked.connect(self.load_sample_pack)
-		self.play_button.clicked.connect(self.session_thread)
-		
-		self.show()
-	
-	def session_thread(self, *args, **kwargs):
-		if self.sample_pack_path:
-			thread = Worker(quantizer.Player,
-							self.quantize_radio_button.isChecked(),
-							self.sample_pack_path,
-							self.metronome_radio_button.isChecked(), 
-							self.tempo_slider.value()
-							)
-			self.threadpool.start(thread)
-			self.play_button.setEnabled(False)
-			self.load_sample_pack_button.setEnabled(False)
-			self.play_button.setText('R E A D Y')
-		else:
-			error_dialog = QtWidgets.QErrorMessage()
-			error_dialog.showMessage('Please load a sample pack using the SAMPLES button :)')
-			error_dialog.exec_()
+        self.sample_pack_path = None
 
-	def tempo_value(self):
-		self.tempo_label.setText(f'Tempo: {self.tempo_slider.value()}')
+        self.load_sample_pack_button = self.findChild(
+            QtWidgets.QPushButton, "load_sample_pack_button"
+        )
+        self.tempo_label = self.findChild(QtWidgets.QLabel, "tempo_label")
+        self.tempo_slider = self.findChild(QtWidgets.QSlider, "tempo_slider")
+        self.metronome_radio_button = self.findChild(
+            QtWidgets.QCheckBox, "metronome_radio_button"
+        )
+        self.quantize_radio_button = self.findChild(
+            QtWidgets.QCheckBox, "quantize_radio_button"
+        )
+        self.play_button = self.findChild(QtWidgets.QPushButton, "playtronic_button")
 
-	def load_sample_pack(self):
-		dialog = QtWidgets.QFileDialog(self)
-		folder = dialog.getExistingDirectory()
-		self.sample_pack_path = folder
-		self.load_sample_pack_button.setText(folder.split('/')[-1])
+        self.tempo_slider.valueChanged.connect(self.tempo_value)
+        self.load_sample_pack_button.clicked.connect(self.load_sample_pack)
+        self.play_button.clicked.connect(self.session_thread)
 
-	def keyPressEvent(self, event):
-		if event.key() == Qt.Key_Space:
-			self.play_button.setEnabled(True)
-			self.play_button.setText('P L A Y T R O N')
-			self.load_sample_pack_button.setEnabled(True)
+        self.show()
+
+    def session_thread(self, *args, **kwargs):
+        if self.sample_pack_path:
+            thread = Worker(
+                quantizer.Player,
+                self.quantize_radio_button.isChecked(),
+                self.sample_pack_path,
+                self.metronome_radio_button.isChecked(),
+                self.tempo_slider.value(),
+            )
+            self.threadpool.start(thread)
+            self.play_button.setEnabled(False)
+            self.load_sample_pack_button.setEnabled(False)
+            self.play_button.setText("R E A D Y")
+        else:
+            error_dialog = QtWidgets.QErrorMessage()
+            error_dialog.showMessage(
+                "Please load a sample pack using the SAMPLES button :)"
+            )
+            error_dialog.exec_()
+
+    def tempo_value(self):
+        self.tempo_label.setText(f"Tempo: {self.tempo_slider.value()}")
+
+    def load_sample_pack(self):
+        dialog = QtWidgets.QFileDialog(self)
+        folder = dialog.getExistingDirectory()
+        self.sample_pack_path = folder
+        self.load_sample_pack_button.setText(folder.split("/")[-1])
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.play_button.setEnabled(True)
+            self.play_button.setText("P L A Y T R O N")
+            self.load_sample_pack_button.setEnabled(True)
 
 
-if __name__ == '__main__':
-	app = QtWidgets.QApplication(sys.argv)
-	window = UI()
-	app.exec_()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = UI()
+    app.exec_()
--- quantizer/playtronica_tools.py
+++ quantizer/playtronica_tools.py
@@ -1,34 +1,36 @@
-import pygame.mixer as mixer
-import pygame.midi as midi
 import os
+
+import pygame.midi as midi
+import pygame.mixer as mixer
 
 
 class Input:
-    def get_input():
+    def get_input(self):
         playtron = None
         midi.init()
 
         # Parses available MIDI devices (input and output)
-        devices = [midi.get_device_info(device)
-                   for device in range(0, midi.get_count())]
+        devices = [
+            midi.get_device_info(device) for device in range(0, midi.get_count())
+        ]
 
         # Locates the Playtronic input ID
         if len(devices) > 0:
             for device in devices:
 
                 # Searching for Playtron's name and the INPUT connection
-                if b'Playtron' in device[1] and device[2] == 1:
+                if b"Playtron" in device[1] and device[2] == 1:
                     playtron = midi.Input(devices.index(device))
 
         if playtron:
             return playtron
 
         else:
-            raise ValueError('No Playtron device found connected.')
+            raise ValueError("No Playtron device found connected.")
 
 
 class Samples:
-    def get_samples(sample_pack_path):
+    def get_samples(self, sample_pack_path):
         mixer.init()
         samples = {}
 
@@ -44,8 +46,8 @@ class Samples:
             # The Playtron sends MIDI codes between 36-51
             i = 36
             for file in files:
-                if file.endswith('.mp3') or file.endswith('.wav'):
-                    samples[i] = mixer.Sound(f'{sample_pack_path}/{file}')
+                if file.endswith(".mp3") or file.endswith(".wav"):
+                    samples[i] = mixer.Sound(f"{sample_pack_path}/{file}")
                     i += 1
                 if i >= 52:
                     break
